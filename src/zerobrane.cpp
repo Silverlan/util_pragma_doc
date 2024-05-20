d3b3e78e6ecf374cd1aa225bd3d56f1cc280f942
Silverlan <silverlann@gmail.com>
2024-05-20 06:19:25 +0200

    refactor: change code to use c++20 modules

--- src/zerobrane.cpp
+++ src/zerobrane.cpp
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "util_pragma_doc.hpp"
 #include <sharedutils/util_string.h>
 #include <unordered_set>
 #include <sstream>
+
+module util_pragma_doc;
 
 static std::string normalize_text(const std::string &text)
 {
